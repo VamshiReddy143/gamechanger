efbc16307c5858ba7ca2512c3b211a1c25605c02
vamshi <vamshireddy0726@gmail.com>
2025-09-09 11:52:08 +0530

    ch7

--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -1,5 +1,5 @@
 const  express =require ("express");
-const  { FORGOT_PASSWORD, LOGIN, LOGOUT, REFRESH_TOKEN, REGISTER, RESET_PASSWORD, VERIFY_OTP, RESEND_OTP, ME } = require( "../controllers/auth.controller");
+const  { FORGOT_PASSWORD, LOGIN, LOGOUT, REGISTER, RESET_PASSWORD, VERIFY_OTP, RESEND_OTP, ME } = require( "../controllers/auth.controller");
 
 const router = express.Router();
 
@@ -9,25 +9,9 @@ router.post("/verify-otp", VERIFY_OTP);
 router.post("/resend-otp", RESEND_OTP);
 router.post("/forgot-password", FORGOT_PASSWORD);
 router.post("/reset-password/:token", RESET_PASSWORD);
-router.post("/refresh-token", REFRESH_TOKEN);
 router.post("/logout", LOGOUT);
 router.get("/me",ME)
-// Add these test endpoints to your auth routes
-router.get('/test-cookies', (req, res) => {
-  res.json({
-    cookiesPresent: !!req.cookies.accessToken,
-    cookies: req.cookies
-  });
-});
 
-router.post('/test-cookie', (req, res) => {
-  res.cookie('testCookie', 'testValue', {
-    httpOnly: true,
-    sameSite: 'lax',
-    domain: 'localhost'
-  });
-  res.send('Cookie set');
-});
 
 module.exports = router
 
